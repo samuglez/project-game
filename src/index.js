85f1db096f10d97f9ad1f7b7c294a7f58f33769d
unknown <samuglez90@gmail.com>
2025-01-27 13:14:48 +0100

    Spawn balloons ok / Score and Timer ok

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
 const startButton = document.getElementById("btn-start");
 const startScreen = document.getElementById("start-screen");
 const gameScreen = document.getElementById("game-screen");
+const playScore = document.getElementById("score");
+const playTimer = document.getElementById("timer");
+const endMessage = document.getElementById("end-message");
 const balloons = ["red", "green", "blue", "purple", "black"];
 const balloonLives = {red:5, green:10, blue:15, purple:20, black:25};
+let score = 0;
+let timeLeft = 60;
+let timerInterval;
 
 
 
@@ -10,6 +16,20 @@ function startGame() {
     startButton.addEventListener("click",()=> {
         startScreen.style.display = "none";
         gameScreen.style.display = "block";
+        score = 0
+        timeLeft = 60
+        updateScore()
+        updateTimer()
+        timerInterval = setInterval(() => {
+            timeLeft--;
+            updateTimer();
+            if (timeLeft <= 0) {
+                endGame(false);
+            }
+        }, 1000);
+        spawnBalloons();
+        
+
     
     });
 }
@@ -33,8 +53,27 @@ function createBalloons() {
     gameScreen.appendChild(balloon);
 }
 function popBalloon(balloon) {
-    
+    let life = parseInt(balloon.dataset.life)
+    if(life > 1){
+        life --;
+        balloon.dataset.life = life
+    }else {
+        score++;
+        updateScore();
+        balloon.remove();
+    }
+    if(document.querySelectorAll(".balloon").length === 0){
+        endGame = true
+
+    }
 }
+function updateScore(){
+    playScore.textContent =`Score: ${score}`
+}
+function updateTimer(){
+    playTimer.textContent = `Timer: ${timeLeft}`
+}
+
 
 
 
